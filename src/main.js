2e28374d2aa1864900cb9d7e3f0c6e75287cd6a6
andy <andy.haz3@gmail.com>
2020-06-02 18:08:36 +0900

    Adding vuetify and cue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from '@/plugins/vuetify'
 
 new Vue({
   el: '#app',
+  router,
+  vuetify,
   render: h => h(App)
 })
